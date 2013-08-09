fd95345b4b41931949497f6b83c66340c03c93c2
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2013-08-09 13:36:18 +0200

    Add a folder for dev scripts

---
+++ scripts/dummy_authentication.py
@@ -0,0 +1,8 @@
+# This is a dummy authentication file.
+# Change the variables to your authentication details and the scripts in this
+# folder will work. See scripts/README.rst for more details.
+
+client_id = ""
+client_secret = ""
+access_token = ""
+refresh_token = ""
