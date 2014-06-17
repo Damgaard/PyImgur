2f4aa2531b3037e9e5f79ca9df5e612976bd31d0
Tobias Megies <megies@geophysik.uni-muenchen.de>
2014-06-17 22:40:20 +0200

    str.format() py2.6 compatibility
    and some possible bugfixes (two times list provided to .format and one
    additional bracket)

--- pyimgur/request.py
+++ pyimgur/request.py
@@ -93,7 +93,7 @@ def send_request(url, params=None, method='GET', data_field='data',
         content = content[data_field]
     if not resp.ok:
         try:
-            error_msg = "Imgur ERROR message: {}".format(content['error'])
+            error_msg = "Imgur ERROR message: {0}".format(content['error'])
             print(error_msg)
             print("-" * len(error_msg))
         except Exception:
