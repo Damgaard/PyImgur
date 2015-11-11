10dc83c6bc3888de92d6dff7af613d4854ff2c6e
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2015-11-11 16:47:48 +0100

    Add a test for get_subreddit_gallery fix

---
+++ pyimgur/test/gallery_test.py
@@ -0,0 +1,38 @@
+# This file is part of PyImgur.
+
+# PyImgur is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# PyImgur is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with PyImgur.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+
+sys.path.insert(0, ".")
+
+import pytest
+
+from authentication import client_id
+import pyimgur
+
+# Make im protected, so it's not run on initialization
+im = pyimgur.Imgur(client_id)
+
+
+def test_get_subreddit():
+    response = im.get_subreddit_gallery("pic", limit=5)
+    assert isinstance(response[0], pyimgur.Image) or isinstance(response[0], pyimgur.Album)
+
+
+def test_get_subreddit_gallery_low_limit():
+    requested_limit = 5
+    response = im.get_subreddit_gallery("pic", limit=requested_limit)
+    assert len(response) == requested_limit
+
