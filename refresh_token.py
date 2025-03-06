be2a881c1d92228d29904bd4cc9c6326bd239041
Andreas Damgaard Pedersen <andreas.damgaard.p@gmail.com>
2025-03-06 12:06:44 +0100

    Add helper file to set refresh token

---
+++ refresh_token.py
@@ -0,0 +1,43 @@
+"""Helper function to get refresh tokens
+
+These are needed both in the general testing library and for
+general development of library.
+
+"""
+
+import os
+
+import pyimgur
+
+def get_refresh_token():
+    im = pyimgur.Imgur(client_id, client_secret)
+    auth_url = im.authorization_url('pin')
+
+    print("Go to the following url to authenticate with your app")
+    print(auth_url)
+
+    pin = input("What is the pin? ")
+    access_token, refresh_token = im.exchange_pin(pin)
+
+    print("You have succesfully authenticated")
+    print(f"Refresh Token: {refresh_token}")
+    print(f"Access Token: {access_token}")
+
+    with open("authentication.py", "w") as outfile:
+        outfile.write(f'client_id = "{client_id}"\n')
+        outfile.write(f'client_secret = "{client_secret}"\n')
+        outfile.write(f'refresh_token = "{refresh_token}"\n')
+
+    print()
+    print("Authentication.py file has been created with credentials.")
+    print("It is needed for test suite and for development.")
+
+if __name__ == "__main__":
+    if not os.path.exists("authentication.py"):
+        print("ERROR: Cannot get refresh token without knowing client_id and secret")
+        print("Create a file called authentication.py and set client_id and")
+        print("client_secret in it.")
+    
+    else:
+        from authentication import client_id, client_secret
+        get_refresh_token()
