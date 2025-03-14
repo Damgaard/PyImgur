910af9d66714d507b69f2d392649ae499e71b060
Andreas Damgaard Pedersen <andreas.damgaard.p@gmail.com>
2025-03-14 19:51:35 +0100

    Add mocked tests for basic object fetches
    Main goal here is just to track if the url accidentally get's
    mangled or some other thing causes the test to break in an
    unexpected way. This is not meant as a deep testing of it's
    functionality.

--- pyimgur/test/request_test.py
+++ pyimgur/test/request_test.py
@@ -17,12 +17,17 @@
 import responses
 
 import pytest
+
+from pyimgur import Imgur, Album
 from pyimgur.request import send_request
 from pyimgur.exceptions import (
     UnexpectedImgurException,
     InvalidParameterError,
     ImgurIsDownException,
 )
+
+MOCKED_UNAUTHED_IMGUR = Imgur("fake_client_id")
+MOCKED_AUTHED_IMGUR = Imgur("fake_client_id", "fake_client_secret", "fake access token")
 
 
 @responses.activate
@@ -88,3 +93,169 @@ def test_send_request_adds_paramaters():
         content_to_send={"params": {"bool_value": "true"}},
     )
     assert content == "hello world"
+
+
+@responses.activate
+def test_get_image_uses_right_url():
+    image_id = "abc123"
+
+    responses.add(
+        responses.GET,
+        f"https://api.imgur.com/3/image/{image_id}",
+        json={"data": {"id": image_id, "title": "test"}},
+        status=200,
+    )
+
+    im = Imgur("fake_client_id")
+    im.get_image(image_id)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == f"https://api.imgur.com/3/image/{image_id}"
+
+
+@responses.activate
+def test_get_album_uses_right_url():
+    album_id = "xyz789"
+
+    responses.add(
+        responses.GET,
+        f"https://api.imgur.com/3/album/{album_id}",
+        json={"data": {"id": album_id, "title": "test"}},
+        status=200,
+    )
+
+    im = Imgur("fake_client_id")
+    im.get_album(album_id)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == f"https://api.imgur.com/3/album/{album_id}"
+
+
+@responses.activate
+def test_get_message_uses_right_url():
+    message_id = "msg123"
+
+    responses.add(
+        responses.GET,
+        f"https://api.imgur.com/3/message/{message_id}",
+        json={"data": {"id": message_id, "content": "test"}},
+        status=200,
+    )
+
+    im = Imgur("fake_client_id")
+    im.get_message(message_id)
+
+    assert len(responses.calls) == 1
+    assert (
+        responses.calls[0].request.url
+        == f"https://api.imgur.com/3/message/{message_id}"
+    )
+
+
+@responses.activate
+def test_get_notification_uses_right_url():
+    notification_id = "notif456"
+
+    responses.add(
+        responses.GET,
+        f"https://api.imgur.com/3/notification/{notification_id}",
+        json={"data": {"id": notification_id, "content": "test"}},
+        status=200,
+    )
+
+    im = Imgur("fake_client_id")
+    im.get_notification(notification_id)
+
+    assert len(responses.calls) == 1
+    assert (
+        responses.calls[0].request.url
+        == f"https://api.imgur.com/3/notification/{notification_id}"
+    )
+
+
+@responses.activate
+def test_get_user_uses_right_url():
+    username = "test_user"
+
+    responses.add(
+        responses.GET,
+        f"https://api.imgur.com/3/account/{username}",
+        json={"data": {"url": username, "id": 12345}},
+        status=200,
+    )
+
+    im = Imgur("fake_client_id")
+    im.get_user(username)
+
+    assert len(responses.calls) == 1
+    assert (
+        responses.calls[0].request.url == f"https://api.imgur.com/3/account/{username}"
+    )
+
+
+@responses.activate
+def test_album_delete_calls_right_url():
+    album_id = "xyz789"
+
+    responses.add(
+        responses.DELETE,
+        f"https://api.imgur.com/3/album/{album_id}",
+        json={"data": True},
+        status=200,
+    )
+
+    album = Album(
+        {"id": album_id, "deletehash": "deletehash", "title": "test"},
+        MOCKED_AUTHED_IMGUR,
+        True,
+    )
+    album.delete()
+
+    assert responses.calls[0].request.url == f"https://api.imgur.com/3/album/{album_id}"
+    assert responses.calls[0].request.method == "DELETE"
+
+
+@responses.activate
+def test_album_delete_unauthed_imgur_uses_deletehash():
+    deletehash = "deletehash"
+
+    responses.add(
+        responses.DELETE,
+        f"https://api.imgur.com/3/album/{deletehash}",
+        json={"data": True},
+        status=200,
+    )
+
+    album = Album(
+        {"id": "album_id", "deletehash": deletehash, "title": "test"},
+        MOCKED_UNAUTHED_IMGUR,
+        True,
+    )
+    album.delete()
+
+    assert (
+        responses.calls[0].request.url == f"https://api.imgur.com/3/album/{deletehash}"
+    )
+    assert responses.calls[0].request.method == "DELETE"
+
+
+@responses.activate
+def test_album_favorite_calls_right_url():
+    album_id = "xyz789"
+
+    responses.add(
+        responses.POST,
+        f"https://api.imgur.com/3/album/{album_id}/favorite",
+        json={"data": {"id": album_id, "title": "test"}},
+        status=200
+    )
+
+    album = Album(
+        {"id": album_id, "deletehash": "deletehash", "title": "test"},
+        MOCKED_AUTHED_IMGUR,
+        True
+    )
+    album.favorite()
+
+    assert responses.calls[0].request.url == f"https://api.imgur.com/3/album/{album_id}/favorite"
+    assert responses.calls[0].request.method == "POST"
