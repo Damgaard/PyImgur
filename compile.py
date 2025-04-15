d502e64a4da556c3e0b85ab8cd136c4dd69e7781
Andreas Damgaard Pedersen <andreas.damgaard.p@gmail.com>
2025-04-15 21:43:37 +0200

    #Irony

--- compile.py
+++ compile.py
@@ -1,3 +1,20 @@
+# This file is part of PyImgur.
+
+# PyImgur is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# PyImgur is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with PyImgur.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Compile stage. Run after cursor change to ensure quality."""
+
 import subprocess
 
 import os
@@ -15,11 +32,12 @@ subprocess.run(
 
 # Next everything is linted
 print("Linting package...")
-subprocess.run(r"pylint pyimgur", shell=True, check=True)
+subprocess.run(r"pylint pyimgur *.py", shell=True, check=True)
 
 print("Linting tests...")
 subprocess.run(
-    r"pylint tests --disable missing-function-docstring,missing-module-docstring,protected-access,missing-class-docstring",
+    r"pylint tests --disable missing-function-docstring,missing-module-docstring,"
+    "protected-access,missing-class-docstring",
     shell=True,
     check=True,
 )
